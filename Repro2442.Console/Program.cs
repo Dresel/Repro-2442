fceff526199166bff2d0e15d03697bbabfc533cf
Christopher Dresel <christopher.dresel@outlook.com>
2019-03-13 23:35:08 +0100

    Refactored to Microsoft.Build.Locator

--- Repro2442.Console/Program.cs
+++ Repro2442.Console/Program.cs
@@ -1,16 +1,31 @@
 ﻿namespace Repro2442.Console
 {
-	using System;
+	using System.Linq;
 	using Microsoft.Build.Evaluation;
+	using Microsoft.Build.Locator;
 
 	public class Program
 	{
 		private static void Main(string[] args)
 		{
-			Environment.SetEnvironmentVariable(MSBuildFinder.MSBuildEnvironmentVariableName, MSBuildFinder.Find());
+			////Environment.SetEnvironmentVariable(MSBuildFinder.MSBuildEnvironmentVariableName, MSBuildFinder.Find());
+			RegisterMSBuild();
+			LoadProjectCollection();
+		}
 
+		private static void LoadProjectCollection()
+		{
 			ProjectCollection projectCollection = new ProjectCollection();
 			Project project = projectCollection.LoadProject("..\\..\\..\\Repro2442.Console.csproj");
+		}
+
+		private static void RegisterMSBuild()
+		{
+			MSBuildLocator.RegisterInstance(MSBuildLocator.QueryVisualStudioInstances(
+				new VisualStudioInstanceQueryOptions()
+				{
+					DiscoveryTypes = DiscoveryType.DotNetSdk
+				}).SingleOrDefault());
 		}
 	}
 }
